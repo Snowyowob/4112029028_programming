9d6de3b64dee174338e0ea9ebb8c44097c3e38c3
Snowyowob <144210390+Snowyowob@users.noreply.github.com>
2023-09-06 16:11:23 +0800

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+w = float(input("Enater your weight (kilogram):"))
+h = float(input("Enater your height (meter):"))
+bmi = w/(h**2)
+print(f"Your BMI is:{bmi:.2f}")
